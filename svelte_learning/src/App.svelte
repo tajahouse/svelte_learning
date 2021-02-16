e26db0f111c63a914902736cb112390b14aebcad
Taja House <ahlliahtaja@yahoo.com>
2021-02-15 21:44:19 -0800

    learned to implement funcbutton

--- svelte_learning/src/App.svelte
+++ svelte_learning/src/App.svelte
@@ -1,30 +1,17 @@
 <script>
-	export let name;
+	export let name, age;
+
+	// increment age function
+	const incrementAge = () => age++
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello {name}! I am {age} years old.</h1>
+	<button on:click="{incrementAge}"> Change the age</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+h1{
+	color: purple;
+}
 </style>
