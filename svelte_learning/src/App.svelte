e0f7d3258f7f1559e428831510826c56b2dc1720
Taja House <ahlliahtaja@yahoo.com>
2021-02-15 22:21:59 -0800

    Learned input method

--- svelte_learning/src/App.svelte
+++ svelte_learning/src/App.svelte
@@ -1,17 +1,41 @@
 <script>
 	export let name, age;
 
+	//assign variable using reactive statements
+	$: uppercaseName = name.toUpperCase();
+
 	// increment age function
 	const incrementAge = () => age++
+
+	//change name
+	const changeName = () => name === 'Taja'? name = 'LadyLotus': name = 'Taja'
+	
+	//input name function
+	const nameInput = (e) => {
+		e.preventDefault()
+		name = e.target.value
+	}
+
+	//Use reactive variables to console.log to view state
+	$: console.log('name',name)
+	$: console.log('age',age)
+
 </script>
 
 <main>
-	<h1>Hello {name}! I am {age} years old.</h1>
+	<h1>Hello {uppercaseName}! I am {age} years old.</h1>
 	<button on:click="{incrementAge}"> Change the age</button>
+	<button on:click="{changeName}">Change Name</button>
+	<input type="text" value="{name}" on:input="{nameInput}">
 </main>
 
 <style>
 h1{
 	color: purple;
+}
+button{
+	color:yellow;
+	border-radius: 8%;
+	background-color: cornflowerblue;
 }
 </style>
